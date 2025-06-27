e73b75a2a5f0c0159faaf833bea6ad6175d72e7b
KumananEswaran <kumanan1731@gmail.com>
2025-06-27 15:01:16 +0800

    add post request to add a new task

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 });
 
+// To add a new task
+app.post('/add-task', async (req, res) => {
+	const { title, description, status } = req.body;
+
+	if (!title || !description || !status) {
+		return res.status(400).json({ error: 'All fields are required' });
+	}
+
+	try {
+		await pool.query(
+			`INSERT INTO tasks (title, description, status) VALUES ($1, $2, $3) RETURNING *`,
+			[title, description, status]
+		);
+		res.status(201).json({ message: `Task added successfully` });
+	} catch (error) {
+		console.error('Error:', error);
+		res.status(500).send({ error: error.message });
+	}
+});
+
 app.get('/', (req, res) => {
 	res.send('Welcome to the Express API!');
 });
